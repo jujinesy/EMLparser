42b2ad1e6c38f923c48fd4a119acbdcbbe7a7490
Ju Jin BAE <jujinesy@gmail.com>
2015-10-13 15:20:46 +0900

    init

---
+++ EMLparser/EML.cpp
@@ -0,0 +1,36 @@
+﻿#include "stdafx.h"
+#include "EML.h"
+
+
+EML::EML()
+{
+}
+
+
+EML::~EML()
+{
+}
+
+void EML::출발()
+{
+	setlocale(LC_ALL, "korean");
+	CStdioFile rfile, wfile;
+	CString data_str;
+	std::regex pattern("\\w*\t([0-9]{4})""/([0-9]{2})""/([0-9]{2})"" ([0-9]{2})"":([0-9]{2})"":([0-9]{2})""\t([\\w]*):""[a-zA-Z0-9\\s]*\\[[a-zA-Z0-9\\s]*\\][a-zA-Z0-9\\s]*\\[(\\S*)\\]"" ([a-zA-Z0-9\\s\\[\\]\\.]*)");
+	std::string replacer = "$7,$8,$1³â $2¿ù $3ÀÏ $4½Ã $5ºÐ $6ÃÊ,$9\n";
+	if (rfile.Open(_T("test.eml"), CFile::modeRead | CFile::typeText))
+	{
+		wfile.Open((CString)"Q_1.csv", CFile::modeCreate | CFile::modeNoTruncate | CFile::modeReadWrite);
+		while (rfile.ReadString(data_str))
+		{
+			CT2CA pszConvertedAnsiString(data_str);
+			wfile.WriteString((CString)std::regex_replace((std::string)pszConvertedAnsiString, pattern, replacer).c_str());
+		}
+		rfile.Close();
+		wfile.Close();
+	}
+	else
+	{
+		MessageBoxW(0, _T("test.eml이 없어요"), 0, 0);
+	}
+}
---
+++ EMLparser/EML.h
@@ -0,0 +1,14 @@
+#pragma once
+#define _AFXDLL
+#include <afx.h>
+#include <regex>
+#include <string>
+
+class EML
+{
+public:
+	EML();
+	~EML();
+	void √‚πﬂ();
+};
+
